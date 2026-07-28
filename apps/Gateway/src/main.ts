bd928f07849d868f8509020542e41f02cfc0e966
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2026-07-28 19:41:19 +0330

    gateway startup

--- apps/Gateway/src/main.ts
+++ apps/Gateway/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>('port', 3000);
+  await app.listen(port);
+  console.log(`Server is running on port ${port}`);
 }
 bootstrap();
